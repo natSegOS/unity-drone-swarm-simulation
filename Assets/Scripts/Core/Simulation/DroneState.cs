505b950da98e1a976dbc22acbf640808227d5232
Nathan Segura <nathan.es@icloud.com>
2025-11-30 17:55:12 -0800

    DSS-13: Created DroneState file skeleton

---
+++ Assets/Scripts/Core/Simulation/DroneState.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DroneSwarmSimulation.Core.Simulation
+{
+    /// <summary>
+    /// Represents the simulation state of a single drone, independent of Unity's scene objects
+    /// </summary>
+    public class DroneState
+    {
+        
+    }
+}
